e3fdf0eba800636c54d40b23526697d428d0fb50
addy <addy@gmail.com>
2024-10-12 11:32:29 -0400

    models completed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import connectDB from "./config/db";
+import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 
 // Load environment variables from .env
@@ -8,8 +8,15 @@ dotenv.config();
 // connect database
 connectDB();
 
-const PORT = process.env.PORT;
+// Config express
+const app = express();
 
-application.listen(PORT, () => {
+app.get("/home", (req, res) => {
+  res.send("Welcome to my app");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
